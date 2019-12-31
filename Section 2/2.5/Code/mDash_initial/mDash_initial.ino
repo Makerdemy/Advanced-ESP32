140802d0ccfc36c4fd6311e332a185b36ca04e28
Makerdemy <36653765+Makerdemy@users.noreply.github.com>
2019-12-31 09:25:57 +0530

    Update mDash_initial.ino

--- Section 2/2.5/Code/mDash_initial/mDash_initial.ino
+++ Section 2/2.5/Code/mDash_initial/mDash_initial.ino
@@ -1,14 +1,17 @@
-#include <mDash.h>
+#define MDASH_APP_NAME "MinimalApp" //This will be shown on dashboard as the Running program's name
+#include <mDash.h> 
+#include <WiFi.h> //Unlike old mdash, the inlusion of this library makes integration of projects easy 
+#define WIFI_NETWORK " " //Add SSID
+#define WIFI_PASSWORD " " //Add Password
+#define DEVICE_PASSWORD " " //Copy device token ID from the dashboard and paste it here 
 
-#define WIFI_NAME "ESPTEST"
-#define WIFI_PASS "chooka123"
-#define DEVICE_ID "d4"
-#define DEVICE_TOKEN "PJRDlLZV1xsYsBvLHVNL0g"
-
-void setup() {
-  Serial.begin(115200);
-  mDashStartWithWifi(WIFI_NAME, WIFI_PASS, DEVICE_ID, DEVICE_TOKEN);
-}
-void loop() {
-  delay(1000);
-}
+void setup() 
+ {  
+    Serial.begin(115200);
+    WiFi.begin(WIFI_NETWORK, WIFI_PASSWORD); 
+    mDashBegin(DEVICE_PASSWORD); //Used instead of mDashStartWithWifi
+   } 
+void loop() 
+   {  
+    delay(100);
+   }
